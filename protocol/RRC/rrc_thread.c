7fca36c23bf2979022db6938c148c7396768ce6b
smarttechliubo <bo.liu@smartlogictech.com>
2019-08-08 15:28:24 +0800

    20190808: add rrc file to the project

---
+++ protocol/RRC/rrc_thread.c
@@ -0,0 +1,14 @@
+/** @file rrc_thread.c
+ *
+ *  @brief
+ *  @author bo.liu
+ *  @date   2019/08/08
+ *  COPYRIGHT NOTICE: (c) smartlogictech.
+ *  All rights reserved.
+ */
+
+int main()
+{
+
+	return 0;
+}
